131288bf3400c80d9500778a55b00f63d7134c5a
Andreas Kobal <kobalandreas@gmail.com>
2021-07-27 00:35:08 +0300

    init project with tailwind

--- src/components/header.js
+++ src/components/header.js
@@ -2,30 +2,14 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
+import Example from "./styledHeader";
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+  <header>
+      <Example/>
+    <div>
+      <h1>
+        <Link to="/">{siteTitle}</Link>
       </h1>
     </div>
   </header>
